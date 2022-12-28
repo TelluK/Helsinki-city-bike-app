913781a6f0684a1b2b0814f360b0c3b20ba14b23
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2022-12-28 18:55:09 +0200

    start and dev scipts can start simple web server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.json('Hello from city bikes!')
+})
+
+module.exports = app
