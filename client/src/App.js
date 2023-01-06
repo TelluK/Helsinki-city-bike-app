a32149c62b40b701010733de167f862e75f6c907
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-06 19:57:03 +0200

    refactor: stations component is separate and used in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,10 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-
-const getStations = (baseUrl) => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
-};
+import Stations from './components/Stations';
 
 const App = () => {
-  const [stations, setStations] = useState([]);
-  const baseUrl = 'http://localhost:3001/api/stations';
-
-  useEffect(() => {
-    getStations(baseUrl)
-      .then((stations) => setStations(stations))
-      .catch((error) => console.log('Error', error));
-  }, []);
-
   return (
-    <div>
-      <h2>List stations</h2>
-      <div>
-        {stations.map((s) => (
-          <p>
-            {s.FID} {s.ID} {s.Nimi} {s.Namn} {s.Name} {s.Osoite} {s.Kaupunki}
-          </p>
-        ))}
-      </div>
-    </div>
+    <>
+      <Stations />
+    </>
   );
 };
 
---
+++ client/src/components/Stations.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const getStations = (baseUrl) => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const Stations = () => {
+  const [stations, setStations] = useState([]);
+  const baseUrl = 'http://localhost:3001/api/stations';
+
+  useEffect(() => {
+    getStations(baseUrl)
+      .then((stations) => setStations(stations))
+      .catch((error) => console.log('Error', error));
+  }, []);
+
+  return (
+    <div>
+      <h2>City Bike stations</h2>
+      <table>
+        <tr>
+          <th>Nimi</th>
+          <th>Namn</th>
+          <th>Name</th>
+          <th>Osoite</th>
+          <th>Kaupunki</th>
+        </tr>
+        {stations.map((s) => (
+          <tr key={s.ID}>
+            <td>{s.Nimi}</td>
+            <td>{s.Namn}</td>
+            <td>{s.Name}</td>
+            <td>{s.Osoite}</td>
+            <td>{s.Kaupunki}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
+
+export default Stations;
