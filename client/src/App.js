0babfbb4d2a74fbb8d343bc8a02a71d025089f4b
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-06 18:26:57 +0200

    client side can query stations from server side and show station information

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,33 @@
-function App() {
-  return <div></div>;
-}
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const getStations = (baseUrl) => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const App = () => {
+  const [stations, setStations] = useState([]);
+  const baseUrl = 'http://localhost:3001/api/stations';
+
+  useEffect(() => {
+    getStations(baseUrl)
+      .then((stations) => setStations(stations))
+      .catch((error) => console.log('Error', error));
+  }, []);
+
+  return (
+    <div>
+      <h2>List stations</h2>
+      <div>
+        {stations.map((s) => (
+          <p>
+            {s.FID} {s.ID} {s.Nimi} {s.Namn} {s.Name} {s.Osoite} {s.Kaupunki}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
