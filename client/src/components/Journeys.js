bd0f950c3e870c05d3f65cdb04f9816e0947c2e4
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-08 20:46:02 +0200

    add MUI table to Journeys component and use TablePagination component

--- client/src/components/Journeys.js
+++ client/src/components/Journeys.js
@@ -1,76 +1,79 @@
 import { useState, useEffect } from 'react';
 import { getJourneys } from '../services/journeys';
+import Container from '@mui/material/Container';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TablePagination from './TablePagination';
 
 const Journeys = () => {
-  const [page, setPage] = useState(1);
-  const [pageCount, setPageCount] = useState(0);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [tableRowCount, setTableRowCount] = useState(0);
   const [journeys, setJourneys] = useState([]);
-  const url = `http://localhost:3001/api/journeys?page=${page}`;
+  const url = `http://localhost:3001/api/journeys?page=${pageNumber}`;
 
   useEffect(() => {
     getJourneys(url)
       .then((data) => {
         setJourneys(data.journeys);
-        setPageCount(Math.ceil(data.pagination.pageCount));
+        setTableRowCount(data.pagination.journeysCount);
       })
       .catch((error) => console.log('Error', error));
-  }, [page]);
+  }, [pageNumber]);
 
-  const handlePrevious = () => {
-    setPage((page) => {
-      if (page === 1) return page;
-      return page - 1;
-    });
-  };
-
-  const handleNext = () => {
-    setPage((page) => {
-      if (page === pageCount) return page;
-      return page + 1;
-    });
+  const handlePageChange = (event, newpage) => {
+    setPageNumber(newpage);
   };
 
   return (
-    <div>
+    <Container>
       <h2>City Bike journeys</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th>Departure time</th>
-            <th>Return time</th>
-            <th>Departure station</th>
-            <th>Return station</th>
-            <th>Covered distance (m)</th>
-            <th>Duration (s)</th>
-          </tr>
-          {journeys.map((journey) => (
-            <tr key={journey.ID}>
-              <td>{journey.departureTime}</td>
-              <td>{journey.returnTime}</td>
-              <td>{journey.departureStationName}</td>
-              <td>{journey.returnStationName}</td>
-              <td>{journey.coveredDistanceM}</td>
-              <td>{journey.durationSec}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        <br />
-        On page: {page}
-        <br />
-        Total page count: {pageCount}
-        <br />
-      </div>
-      <div>
-        <button disabled={page === 1} onClick={handlePrevious}>
-          Previous page
-        </button>
-        <button disabled={page === pageCount} onClick={handleNext}>
-          Next page
-        </button>
-      </div>
-    </div>
+      <TableContainer component={Paper} elevation={10}>
+        <TablePagination
+          page={pageNumber}
+          tableRowCount={tableRowCount}
+          handlePageChange={handlePageChange}
+        />
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Departure time</TableCell>
+              <TableCell align="left">Return time</TableCell>
+              <TableCell align="left">Departure station</TableCell>
+              <TableCell align="left">Return station</TableCell>
+              <TableCell align="left">Covered distance (m)</TableCell>
+              <TableCell align="left">Duration (s)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {journeys.map((journey) => (
+              <TableRow
+                key={journey.ID}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell align="left">{journey.departureTime}</TableCell>
+                <TableCell align="left">{journey.returnTime}</TableCell>
+                <TableCell align="left">
+                  {journey.departureStationName}
+                </TableCell>
+                <TableCell align="left">{journey.returnStationName}</TableCell>
+                <TableCell align="left">{journey.coveredDistanceM}</TableCell>
+                <TableCell align="left">{journey.durationSec}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          page={pageNumber}
+          tableRowCount={tableRowCount}
+          handlePageChange={handlePageChange}
+        />
+      </TableContainer>
+    </Container>
   );
 };
 
