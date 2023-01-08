aafd8618c72824f2214ca425d26b62946960835d
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-08 19:34:04 +0200

    use TablePagination on Stations component, alter page numbering to start from 0

--- client/src/components/Stations.js
+++ client/src/components/Stations.js
@@ -8,34 +8,25 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TablePagination from './TablePagination';
 
 const Stations = () => {
-  const [page, setPage] = useState(1);
-  const [pageCount, setPageCount] = useState(0);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [tableRowCount, setTableRowCount] = useState(0);
   const [stations, setStations] = useState([]);
-  const url = `http://localhost:3001/api/stations?page=${page}`;
+  const url = `http://localhost:3001/api/stations?page=${pageNumber}`;
 
   useEffect(() => {
     getStations(url)
       .then((data) => {
         setStations(data.stations);
-        setPageCount(Math.ceil(data.pagination.pageCount));
+        setTableRowCount(data.pagination.stationsCount);
       })
       .catch((error) => console.log('Error', error));
-  }, [page]);
+  }, [pageNumber]);
 
-  const handlePrevious = () => {
-    setPage((page) => {
-      if (page === 1) return page;
-      return page - 1;
-    });
-  };
-
-  const handleNext = () => {
-    setPage((page) => {
-      if (page === pageCount) return page;
-      return page + 1;
-    });
+  const handlePageChange = (event, newpage) => {
+    setPageNumber(newpage);
   };
 
   return (
@@ -43,6 +34,11 @@ const Stations = () => {
       <Container>
         <h2>City Bike stations</h2>
         <TableContainer component={Paper} elevation={10}>
+          <TablePagination
+            page={pageNumber}
+            tableRowCount={tableRowCount}
+            handlePageChange={handlePageChange}
+          />
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow>
@@ -68,22 +64,12 @@ const Stations = () => {
               ))}
             </TableBody>
           </Table>
+          <TablePagination
+            page={pageNumber}
+            tableRowCount={tableRowCount}
+            handlePageChange={handlePageChange}
+          />
         </TableContainer>
-        <div>
-          <br />
-          On page: {page}
-          <br />
-          Total page count: {pageCount}
-          <br />
-        </div>
-        <div>
-          <button disabled={page === 1} onClick={handlePrevious}>
-            Previous page
-          </button>
-          <button disabled={page === pageCount} onClick={handleNext}>
-            Next page
-          </button>
-        </div>
       </Container>
     </div>
   );
