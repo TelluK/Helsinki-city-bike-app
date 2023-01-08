706d1ad99f0d7e46a315b1f9356e4fb7715eb11a
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-08 17:45:02 +0200

    use MUI table in Stations component

--- client/src/components/Stations.js
+++ client/src/components/Stations.js
@@ -1,5 +1,13 @@
 import { useState, useEffect } from 'react';
 import { getStations } from '../services/stations';
+import Container from '@mui/material/Container';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Stations = () => {
   const [page, setPage] = useState(1);
@@ -32,42 +40,51 @@ const Stations = () => {
 
   return (
     <div>
-      <h2>City Bike stations</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th>Nimi</th>
-            <th>Namn</th>
-            <th>Name</th>
-            <th>Osoite</th>
-            <th>Kaupunki</th>
-          </tr>
-          {stations.map((s) => (
-            <tr key={s.ID}>
-              <td>{s.Nimi}</td>
-              <td>{s.Namn}</td>
-              <td>{s.Name}</td>
-              <td>{s.Osoite}</td>
-              <td>{s.Kaupunki}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        <br />
-        On page: {page}
-        <br />
-        Total page count: {pageCount}
-        <br />
-      </div>
-      <div>
-        <button disabled={page === 1} onClick={handlePrevious}>
-          Previous page
-        </button>
-        <button disabled={page === pageCount} onClick={handleNext}>
-          Next page
-        </button>
-      </div>
+      <Container>
+        <h2>City Bike stations</h2>
+        <TableContainer component={Paper} elevation={10}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Nimi</TableCell>
+                <TableCell align="left">Namn</TableCell>
+                <TableCell align="left">Name</TableCell>
+                <TableCell align="left">Osoite</TableCell>
+                <TableCell align="left">Kaupunki</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {stations.map((station) => (
+                <TableRow
+                  key={station.ID}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="left">{station.Nimi}</TableCell>
+                  <TableCell align="left">{station.Namn}</TableCell>
+                  <TableCell align="left">{station.Name}</TableCell>
+                  <TableCell align="left">{station.Osoite}</TableCell>
+                  <TableCell align="left">{station.Kaupunki}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <div>
+          <br />
+          On page: {page}
+          <br />
+          Total page count: {pageCount}
+          <br />
+        </div>
+        <div>
+          <button disabled={page === 1} onClick={handlePrevious}>
+            Previous page
+          </button>
+          <button disabled={page === pageCount} onClick={handleNext}>
+            Next page
+          </button>
+        </div>
+      </Container>
     </div>
   );
 };
