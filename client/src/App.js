9ae17dd49ae0e7a4834f84c3bfd7c7c0fddeede6
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-10 21:28:30 +0200

    add client side route for path /stations/:id, that uses StationDetails component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Journeys from './components/Journeys';
 import Navbar from './components/Navbar';
 import Stations from './components/Stations';
+import StationDetails from './components/StationDetails';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/journeys" element={<Journeys />} />
         <Route path="/stations" element={<Stations />} />
+        <Route path="/stations/:id" element={<StationDetails />} />
       </Routes>
     </>
   );
