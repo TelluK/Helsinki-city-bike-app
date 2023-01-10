bf2a0256f6f162b0a419509dbd40e74c529b2185
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-10 21:25:00 +0200

    add getSingleStation function in services in client side

--- client/src/services/stations.js
+++ client/src/services/stations.js
@@ -5,3 +5,8 @@ export const getStations = (url) => {
   const request = axios.get(url);
   return request.then((response) => response.data);
 };
+
+export const getSingleStation = (stationID) => {
+  const request = axios.get(`http://localhost:3001/api/stations/${stationID}`);
+  return request.then((response) => response.data);
+};
