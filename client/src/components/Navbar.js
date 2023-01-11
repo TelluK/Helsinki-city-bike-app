c33db13283f11f8cd162ceac87bb12b4e5d1a3e4
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-11 15:29:12 +0200

    style NavBar with Material UI components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,40 @@
 import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import { styled } from '@mui/material/styles';
+import MenuItem from '@mui/material/MenuItem';
 
 const Navbar = () => {
+  const StyledToolbar = styled(Toolbar)({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  });
+
+  const StyledBox = styled(Box)({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  });
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/journeys"> Journeys </Link>
-      <Link to="/stations"> Stations </Link>
-    </nav>
+    <AppBar position={'static'}>
+      <StyledBox></StyledBox>
+      <StyledToolbar>
+        <StyledBox>
+          <MenuItem component={Link} to="/">
+            City Bike
+          </MenuItem>
+          <MenuItem component={Link} to="/journeys">
+            Journeys
+          </MenuItem>
+          <MenuItem component={Link} to="/stations">
+            Stations
+          </MenuItem>
+        </StyledBox>
+      </StyledToolbar>
+    </AppBar>
   );
 };
 
