3e25b562c17855aa80f369bddf0fc3403d959a2e
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-12 22:23:38 +0200

    add routing to Home page in App

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import Journeys from './components/Journeys';
 import Navbar from './components/Navbar';
 import Stations from './components/Stations';
 import StationDetails from './components/StationDetails';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/journeys" element={<Journeys />} />
         <Route path="/stations" element={<Stations />} />
         <Route path="/stations/:id" element={<StationDetails />} />
