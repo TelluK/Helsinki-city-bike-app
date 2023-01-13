51848a0e3675904ac201fc9f9f5ef95ecb18498d
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-13 21:29:14 +0200

    add getJourneyStatsForStation function in journey services

--- client/src/services/journeys.js
+++ client/src/services/journeys.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
+const baseUrl = 'http://localhost:3001/api/journeys';
 
 // get journeys information from server
 export const getJourneys = (url) => {
+  const request = axios.get(url);
+  return request.then((response) => response.data);
+};
+
+// get journey stats for one station from server
+export const getJourneyStatsForStation = (stationID) => {
+  const url = `${baseUrl}/${stationID}`;
   const request = axios.get(url);
   return request.then((response) => response.data);
 };
