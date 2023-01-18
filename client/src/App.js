7d005df3400ede8d8c3e6a554d218e66bdeab47f
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-01-18 20:58:29 +0200

    add Footer to app, now shows links to data licenses and app source code

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,23 @@ import Navbar from './components/Navbar';
 import Stations from './components/Stations';
 import StationDetails from './components/StationDetails';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 const App = () => {
+  const gitHubUrl = 'https://github.com/TelluK/Helsinki-city-bike-app';
+  const stationsDataInfoAndLicense =
+    'https://www.avoindata.fi/data/en_GB/dataset/hsl-n-kaupunkipyoraasemat';
+  const journeysDataInfoAndLicense =
+    'https://www.avoindata.fi/data/en_GB/dataset/helsingin-ja-espoon-kaupunkipyorilla-ajatut-matkat';
   return (
-    <>
+    <div
+      style={{
+        textAlign: 'center',
+        minHeight: '98vh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -15,7 +28,12 @@ const App = () => {
         <Route path="/stations" element={<Stations />} />
         <Route path="/stations/:id" element={<StationDetails />} />
       </Routes>
-    </>
+      <Footer
+        stationsDataInfoAndLicense={stationsDataInfoAndLicense}
+        journeysDataInfoAndLicense={journeysDataInfoAndLicense}
+        gitHubUrl={gitHubUrl}
+      />
+    </div>
   );
 };
 
